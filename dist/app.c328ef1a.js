988d73191c8d4bb64cc5f5699e71102db722567b
sh414lim <77113306+sh414lim@users.noreply.github.com>
2022-11-16 23:24:48 +0900

    ts setting

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,36 +123,72 @@ var ajax = new XMLHttpRequest(); // 출력결과 반환
 var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 var content = document.createElement("div");
+var store = {
+  currentPage: 1,
+  feeds: []
+};
 var getDate = function getDate(url) {
   ajax.open("GET", url, false);
   ajax.send();
   return JSON.parse(ajax.response);
 };
+function makeFeeds(feeds) {
+  for (var i = 0; i < feeds.length; i++) {
+    feeds[i].read = false;
+  }
+  return feeds;
+}
 function newsFeed() {
-  var newsFeed = getDate(NEWS_URL);
+  var newsFeed = store.feeds;
   var newsList = [];
-  newsList.push("<ul>");
-  for (var i = 0; i < 10; i++) {
+  var templage = "\n  <div class=\"bg-gray-600 min-h-screen\">\n    <div class=\"bg-white text-xl\">\n      <div class=\"mx-auto px-4\">\n        <div class=\"flex justify-between items-center py-6\">\n          <div class=\"flex justify-start\">\n            <h1 class=\"font-extrabold\">Hacker News</h1>\n          </div>\n          <div class=\"items-center justify-end\">\n            <a href=\"#/page/{{__prev_page__}}\" class=\"text-gray-500\">\n              Previous\n            </a>\n            <a href=\"#/page/{{__next_page__}}\" class=\"text-gray-500 ml-4\">\n              Next\n            </a>\n          </div>\n        </div> \n      </div>\n    </div>\n    <div class=\"p-4 text-2xl text-gray-700\">\n      {{__news_feed__}}        \n    </div>\n  </div>\n";
+  if (newsFeed.length === 0) {
+    newsFeed = store.feeds = makeFeeds(getDate(NEWS_URL));
+  }
+  for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     var title = newsFeed[i].title;
     var desc = newsFeed[i].comments_count;
     var id = newsFeed[i].id;
-    newsList.push("\n      <li>\n        <a href = \"#".concat(id, "\">").concat(title, " - ").concat(desc, "</a>\n      </li>\n      "));
+    newsList.push("\n      <div class=\"p-6 ".concat(newsFeed[i].read ? "bg-red-500" : "bg-white", " mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100\">\n        <div class=\"flex\">\n          <div class=\"flex-auto\">\n            <a href=\"#/show/").concat(newsFeed[i].id, "\">").concat(newsFeed[i].title, "</a>  \n          </div>\n          <div class=\"text-center text-sm\">\n            <div class=\"w-10 text-white bg-green-300 rounded-lg px-0 py-2\">").concat(newsFeed[i].comments_count, "</div>\n          </div>\n        </div>\n        <div class=\"flex mt-3\">\n          <div class=\"grid grid-cols-3 text-sm text-gray-500\">\n            <div><i class=\"fas fa-user mr-1\"></i>").concat(newsFeed[i].user, "</div>\n            <div><i class=\"fas fa-heart mr-1\"></i>").concat(newsFeed[i].points, "</div>\n            <div><i class=\"far fa-clock mr-1\"></i>").concat(newsFeed[i].time_ago, "</div>\n          </div>  \n        </div>\n      </div>    \n    "));
   }
-  newsList.push("</ul>");
-  container.innerHTML = newsList.join(""); // 하나의 문자열 조인
+  templage = templage.replace("{{__news_feed__}}", newsList.join(""));
+  templage = templage.replace("{{__next_page__}}", store.currentPage > 1 ? store.currentPage - 1 : 1);
+  templage = templage.replace("{{__prev_page__}}", store.currentPage < 3 ? store.currentPage + 1 : 3);
+  container.innerHTML = templage; // 하나의 문자열 조인
 }
 
 function newsDetail() {
-  var id = location.hash.substr(1);
+  var id = location.hash.substr(7);
   var url = CONTENT_URL.replace("@id", id);
   var newsContent = getDate(url);
   var subTitle = newsContent.title;
-  container.innerHTML = "\n          <h1>".concat(subTitle, "</h1>\n      \n          <div>\n              <a href=\"#\">back</a>\n          </div>\n        ");
+  var template = "\n  <div class=\"bg-gray-600 min-h-screen pb-8\">\n    <div class=\"bg-white text-xl\">\n      <div class=\"mx-auto px-4\">\n        <div class=\"flex justify-between items-center py-6\">\n          <div class=\"flex justify-start\">\n            <h1 class=\"font-extrabold\">Hacker News</h1>\n          </div>\n          <div class=\"items-center justify-end\">\n            <a href=\"#/page/".concat(store.currentPage, "\" class=\"text-gray-500\">\n              <i class=\"fa fa-times\"></i>\n            </a>\n          </div>\n        </div>\n      </div>\n    </div>\n\n    <div class=\"h-full border rounded-xl bg-white m-6 p-4 \">\n      <h2>").concat(newsContent.title, "</h2>\n      <div class=\"text-gray-400 h-20\">\n        ").concat(newsContent.content, "\n      </div>\n\n      {{__comments__}}\n\n    </div>\n  </div>\n");
+  for (var i = 0; i < store.feeds.length; i++) {
+    if (store.feeds[i] === Number(id)) {
+      store.feeds[i].read = true;
+      break;
+    }
+  }
+  function makeComment(comments) {
+    var called = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    var commentString = [];
+    for (var _i = 0; _i < comments.length; _i++) {
+      commentString.push("\n        <div style=\"padding-left: ".concat(called * 40, "px;\" class=\"mt-4\">\n          <div class=\"text-gray-400\">\n            <i class=\"fa fa-sort-up mr-2\"></i>\n            <strong>").concat(comments[_i].user, "</strong> ").concat(comments[_i].time_ago, "\n          </div>\n          <p class=\"text-gray-700\">").concat(comments[_i].content, "</p>\n        </div>      \n      "));
+      if (comments[_i].comments.length > 0) {
+        commentString.push(makeComment(comments[_i].comments, called + 1));
+      }
+    }
+    return commentString.join("");
+  }
+  container.innerHTML = template.replace("{{__comments__}}", makeComment(newsContent.comments));
 }
 function router() {
   var routePath = location.hash;
   console.log(routePath);
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substr(7));
     newsFeed();
   } else {
     newsDetail();
